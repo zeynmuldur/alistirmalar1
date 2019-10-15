1950538379f0d501014217a2d181e607853213d6
zeynmuldur <56604026+zeynmuldur@users.noreply.github.com>
2019-10-15 22:58:30 +0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,5 +1,9 @@
+#son rakamı çift en az 2 rakamı aynı olan 3 basamaklı sayıları yazdıran program
+sayac=0
 for i in range(1,10) :
     for j in range (0,10):
         for k in range(0,10,2):
             if i==k or i==j or k==j or i==j==k:
                 print(100*i+10*j+k)
+                sayac+=1
+print("su kadar sayı var:",sayac)
