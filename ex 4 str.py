e8b677c6d661109cf8d02500c08b25e9eb8ac99e
zeynmuldur <56604026+zeynmuldur@users.noreply.github.com>
2019-11-06 21:28:58 +0300

    Add files via upload

---
+++ ex 4 str.py
@@ -0,0 +1,4 @@
+for i in range(100,1000):
+    s=str(i)
+    if int(s[0])==int(s[2]) :
+        print(s)
---
+++ ex6 str.py
@@ -0,0 +1,8 @@
+for i in range(1000,10000):
+    s=str(i)
+    sayac=0
+    if (int(s[0])>int(s[3])):
+        print(s)
+        sayac=sayac+1
+              
+print("bu problemi sağlayan",sayac,"tane sayi vardir")
